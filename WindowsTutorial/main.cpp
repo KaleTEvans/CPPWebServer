c6fd3ec48b88ffcbf7f55fcd9c83e4fc48e1945e
Kale Evans <kaletevans@gmail.com>
2022-01-23 17:46:14 -0600

    first try is failing to connect, trying a different method in windows tutorial

---
+++ WindowsTutorial/main.cpp
@@ -0,0 +1,27 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+// when compiling be sure to add -lwsock32
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+
+    int iResult;
+    // initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    return 0;
+}
